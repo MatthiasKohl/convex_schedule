295ebf36e6d87d0689da2eaef8bcfc322b3961e2
Matthias Kohl <matthias.kohl@hotmail.de>
2016-05-25 12:50:17 +0200

    added best fit implementation, needs more testing/bug fixing

--- ffd.py
+++ ffd.py
@@ -6,13 +6,29 @@ import operator
 import itertools
 import random
 from functools import reduce
-from shapes import shape_candidates, char_range
+from shapes import shape_candidates, char_range, metric_compactness
 
 class ConvexSpace:
     def __init__(self, coordinates, boundaries, tag=0):
         self.coordinates = coordinates
         self.boundaries = boundaries
         self.tag = tag
+
+    def __hash__(self):
+        return hash((self.tag, tuple(self.coordinates), tuple(self.boundaries)))
+
+    def __eq__(self, other):
+        if isinstance(other, ConvexSpace):
+            r = self.tag == other.tag
+            r = r and self.coordinates == other.coordinates
+            return r and self.boundaries == other.boundaries
+        return NotImplemented
+
+    def __ne__(self, other):
+        result = self.__eq__(other)
+        if result is NotImplemented:
+            return result
+        return not result
 
     def canFit(self, boundariesToFit):
         for thisS, s in zip(self.boundaries, boundariesToFit):
@@ -113,43 +129,42 @@ class ConvexSpace:
         other.minus(other.intersection(joinedSpace))
         if s == joinedSpace or not joinedSpace.contains(s)]
 
-    # update this space to contain the other space, if it is adjacent or overlapping in one
+    # return joined space containing self and other, if self is adjacent or overlapping in one
     # dimension and all other dimensions overlap entirely
-    # return True if the update was possible, False if not
-    def joinAdjacent(self, other, doJoin = True):
+    # return joined space if the update was possible, None if not
+    def joinAdjacent(self, other):
         if (self.contains(other)):
-            return True
+            return ConvexSpace(list(self.coordinates), list(self.boundaries))
         if (other.contains(self)):
-            if (doJoin):
-                self.coordinates = other.coordinates
-                self.boundaries = other.boundaries
-            return True
+            return ConvexSpace(list(other.coordinates), list(other.boundaries))
         intervals = self.intersectionIntervals(other)
         if (not intervals):
-            return False
+            return None
         # get the dimension over which we join, all others have to overlap completely
         i = -1
         for d in range(len(intervals)):
             if (intervals[d][1] != self.boundaries[d] or intervals[d][1] != other.boundaries[d]):
                 if (i >= 0):
-                    return False
+                    return None
                 i = d
-        if (doJoin):
-            self.coordinates[i] = min(self.coordinates[i], other.coordinates[i])
-            self.boundaries[i] = max(self.coordinates[i] + self.boundaries[i],
-                                      other.coordinates[i] + other.boundaries[i]) - self.coordinates[i]
-        return True
+        newCoordinates = list(self.coordinates)
+        newBoundaries = list(self.boundaries)
+        newCoordinates[i] = min(self.coordinates[i], other.coordinates[i])
+        newBoundaries[i] = max(self.coordinates[i] + self.boundaries[i],
+                                      other.coordinates[i] + other.boundaries[i]) - newCoordinates[i]
+        return ConvexSpace(newCoordinates, newBoundaries)
 
     def __str__(self):
         return str(self.coordinates) + ' -> ' + str(self.boundaries)
 
 class Bin:
     def __init__(self, boundaries):
-        self.freelist = [ConvexSpace([0 for d in boundaries], list(boundaries))]
-        self.spaces = []
+        self.boundaries = boundaries
+        self.freelist = set([ConvexSpace([0 for d in boundaries], list(boundaries))])
+        self.spaces = set()
 
     def canFit(self, boundariesToFit):
-        return any([space.canFit(boundariesToFit) for space in self.freelist])
+        return any(space.canFit(boundariesToFit) for space in self.freelist)
 
     # choose the best join for a flat fit
     # simply try all possible joins and choose the one which leaves the biggest sizes
@@ -182,7 +197,7 @@ class Bin:
                     key=lambda s: [x for d, x in enumerate(s.coordinates) if d != i] + [s.coordinates[i]])
         assignedSpace = ConvexSpace(list(freeSpace.coordinates), list(boundariesToFit))
         #print('Fitting ' + str(boundariesToFit) + ' into ' + str(freeSpace))
-        self.spaces.append(assignedSpace)
+        self.spaces.add(assignedSpace)
         self.freelist.remove(freeSpace)
         newFreeSpaces = set(freeSpace.minus(assignedSpace))
 
@@ -230,67 +245,88 @@ class Bin:
                 joinedSpaces = Bin.chooseBestJoinFlat(fs1, fs2)
                 if (joinedSpaces):
                     #print('Joined spaces: ' + str([str(x) for x in joinedSpaces]))
-                    newFreeSpaces = newFreeSpaces.difference({fs1, fs2}).union(set(joinedSpaces))
+                    newFreeSpaces.difference_update({fs1, fs2})
+                    newFreeSpaces.update(set(joinedSpaces))
                     isUpdated = True
                     break
 
         #print('New free spaces: ' + str([str(x) for x in newFreeSpaces]))
-        self.freelist.extend(newFreeSpaces)
+        self.freelist.update(newFreeSpaces)
 
     # fit the given boundaries at the best location according to caving degree
     # i.e. always fit at a corner and try to fill all dimensions as much as possible
     def fitBest(self, boundariesToFit):
-        chosenFS = None
-        chosenFSCoords = None
-        for space in filter(lambda s: s.canFit(boundariesToFit), self.freelist):
-            # TODO
-            chosenFS = space
-            chosenFSCoords = list(space.coordinates)
+        # get the space with the minimum gap over the most dimensions
+        # for each space, sort the boundaries by gap with boundariesToFIt non-decreasing
+        # then take the minimum over that list (i.e. the space that has min minimal gap,
+        # min 2nd minimal gap...)
+        chosenFS = min(filter(lambda s: s.canFit(boundariesToFit), self.freelist),
+                       key = lambda s: sorted([b - boundariesToFit[d] for d, b in enumerate(s.boundaries)]))
+        # TODO coordinates could be adapted depending on other free spaces
+        # in order to leave more or less convex free space open
+        chosenFSCoords = list(chosenFS.coordinates)
         assignedSpace = ConvexSpace(chosenFSCoords, list(boundariesToFit))
-        self.spaces.append(assignedSpace)
+        self.spaces.add(assignedSpace)
 
-        # make sure that the assigned space is subtracted from all existing FS
-        removedFS =[]
-        addedFS = []
+        print('Fitted bin is as follows:\n' + str(self))
+        # make sure that the assigned space is subtracted from all existing FSs
+        removedFSs = set()
+        addedFSs = set()
         for space in self.freelist:
             if (space.isIntersecting(assignedSpace)):
-                removedFS.append(space)
-                addedFS.extend(space.minus(assignedSpace))
-        for space in removedFS:
-            self.freelist.remove(space)
-        self.freelist.extend(addedFS)
+                removedFSs.add(space)
+                for s in space.minus(assignedSpace):
+                    addedFSs.add(s)
+        self.freelist.difference_update(removedFSs)
+        self.freelist.update(addedFSs)
 
-        # make sure that any adjacent FS are joined
-        removedFS = []
-        for fs1, fs2 in itertools.combinations(self.freelist, 2):
-            if (s1.joinAdjacent(s2)):
-                removedFS.append(s2)
-        for space in removedFS:
-            self.freelist.remove(space)
+        print('Fitted bin is as follows:\n' + str(self))
+        # make sure that any adjacent FSs are joined until no adjacent FSs can be joined
+        # at most this can happen as much as there are FSs in freelist
+        while(True):
+            #print('Fitted bin is as follows:\n' + str(self))
+            removedFSs = set()
+            addedFSs = set()
+            for fs1, fs2 in itertools.combinations(self.freelist, 2):
+                joinedSpace = fs1.joinAdjacent(fs2)
+                if (joinedSpace != None):
+                    removedFSs.add(fs1)
+                    removedFSs.add(fs2)
+                    addedFSs.add(joinedSpace)
+            if (not removedFSs and not addedFSs):
+                break
+            self.freelist.difference_update(removedFSs)
+            self.freelist.update(addedFSs)
+
+        print('Fitted bin is as follows:\n' + str(self))
 
     # simple test to check if a bin configuration is possible according to given boundaries
     # it can be specified that free spaces must not overlap (usually, they can overlap
     # between each other, not with other spaces)
-    def testPossible(self, boundaries, allowOverlappingFS = True, allowAdjFS = True):
+    def testPossible(self, allowOverlappingFS = True, allowAdjFS = True):
         # check that no space goes outside of given boundaries
-        allSpaces = self.freelist + self.spaces
-        boundarySpace = ConvexSpace([0 for x in boundaries], boundaries)
+        allSpaces = list(self.freelist) + list(self.spaces)
+        boundarySpace = ConvexSpace([0 for x in self.boundaries], self.boundaries)
         if (not all(boundarySpace.contains(s) for s in allSpaces)):
+            print('Space ' + str([boundarySpace.contains(s) for s in allSpaces][0]) + ' is out of bounds')
             return False
         # check that there is no intersection between any two spaces
-        overlapSpaces = self.spaces if allowOverlappingFS else allSpaces
+        overlapSpaces = list(self.spaces) if allowOverlappingFS else allSpaces
         for s1, s2 in itertools.combinations(overlapSpaces, 2):
             if (s1.isIntersecting(s2)):
+                print('Space ' + str(s1) + ' is intersecting with ' + str(s2) + ' but must not')
                 return False
         # if free spaces can overlap, still need to check that they don't overlap with assigned
         if (allowOverlappingFS):
             for s1, s2 in itertools.product(self.freelist, self.spaces):
                 if (s1.isIntersecting(s2)):
+                    print('Space ' + str(s1) + ' is intersecting with ' + str(s2) + ' but must not')
                     return False
         if (not allowAdjFS):
             # there must not be any two adjacent free spaces
             for s1, s2 in itertools.combinations(self.freelist, 2):
-                if (s1.joinAdjacent(s2, False)):
+                if (s1.joinAdjacent(s2) != None):
+                    print('Space ' + str(s1) + ' is adjacent with ' + str(s2) + ' but must not')
                     return False
         return True
 
@@ -303,9 +339,23 @@ class Bin:
             s = s + str(space) + '\n'
         return s
 
-def ffd(boundaries, listOfRequestSpaces):
+def chooseCandidateFlat(requestSize, total, shape_candidates, alpha):
+    # get the minimum candidate (minimal size of the flattest candidates)
+    # of all candidates that can hold this requestSize according to alpha
+    # this is the candidate that leaves the biggest convex free space when fit into the bin,
+    # according to the heuristic
+    cutOffSize = min(int(requestSize+requestSize*alpha), total)
+    return min([p for n, projs in shape_candidates.items()
+               if n >= requestSize and n <= cutOffSize for p in projs])
+
+def ffdFlat(boundaries, requestSizes, shape_candidates, alpha):
+    total = reduce(operator.mul, boundaries, 1)
+    # always choose the flattest (and smallest) shape for each request size
+    listOfRequestSpaces = map(lambda r:
+                              chooseCandidateFlat(r, total, shape_candidates, alpha), requestSizes)
     if (not listOfRequestSpaces): return []
-    bins = [Bin(boundaries)]
+    bins = []
+    # sort requested shapes by size and do a first-fit over the bins
     for space in sorted(listOfRequestSpaces, reverse=True):
         # get index of last flat dimension, this is the dimension over which the space needs
         # to be fitted into its bin
@@ -316,31 +366,72 @@ def ffd(boundaries, listOfRequestSpaces):
                 b.fitFlat(space, firstNonFlatD)
                 fittedBin = b
                 isFit = True
+                break
         if (not isFit):
             newBin = Bin(boundaries)
             newBin.fitFlat(space, firstNonFlatD)
             fittedBin = newBin
             bins.append(newBin)
-        if (not fittedBin.testPossible(boundaries, False)):
+        if (not fittedBin.testPossible(False)):
             print('Impossible configuration:\n' + str(fittedBin) + '\n')
             break
 
     return bins
 
-def chooseCandidate(requestSize, total, shape_candidates, alpha):
-    # get the minimum candidate (minimal size of the flattest candidates)
-    # of all candidates that can hold this requestSize according to alpha
-    # this is the candidate that leaves the biggest convex free space when fit into the bin,
-    # according to the heuristic
+# return difference between second lowest metric and lowest metric for all possible shapes
+# for given requestSize and alpha, or 0 if there are too few possible shapes
+# TODO this could also return the ratio ?
+def bestMetricsDelta(requestSize, dimensions, shape_candidates, alpha, metric):
+    total = reduce(operator.mul, dimensions.values(), 1)
     cutOffSize = min(int(requestSize+requestSize*alpha), total)
-    return min([p for n, projs in shape_candidates.items()
-               if n >= requestSize and n <= cutOffSize for p in projs])
+    bestMetrics = sorted([metric(p, False, dimensions) for n, projs in shape_candidates.items()
+    if n >= requestSize and n <= cutOffSize for p in projs])
+    if (len(bestMetrics) < 2):
+        return 0
+    return bestMetrics[1] - bestMetrics[0]
 
-def firstFitDecreasing(boundaries, requestSizes, shape_candidates, alpha):
-    total = reduce(operator.mul, boundaries, 1)
-    listOfRequestSpaces = map(lambda r:
-                              chooseCandidate(r, total, shape_candidates, alpha), requestSizes)
-    return ffd(boundaries, listOfRequestSpaces)
+def ffdBest(dimensions, requestSizes, shape_candidates, alpha):
+    if (not requestSizes):
+        return []
+    # sort the requested sizes by non-increasing size,
+    # then by non-increasing delta of best metrics of possible shapes (sort must be stable)
+    # TODO sorting request sizes by size is not exactly right as we want to consider the shapes
+    # by non-increasing size. this still needs to be fixed
+    sortedSizes = sorted(sorted(requestSizes, reverse=True), key=lambda r:
+                         bestMetricsDelta(r, dimensions, shape_candidates, alpha, metric_compactness),
+                         reverse=True)
+    bins = []
+    total = reduce(operator.mul, dimensions.values(), 1)
+    for size in sortedSizes:
+        # try to fit the different shapes in an existing bin, sorted by metric (best to worst)
+        isFit = False
+        cutOffSize = min(int(size+size*alpha), total)
+        spaces = sorted([p for n, projs in shape_candidates.items()
+                        if n >= size and n <= cutOffSize for p in projs], key=lambda p:
+                            metric_compactness(p, False, dimensions))
+        if (not spaces):
+            print('Cannot allocate size ' + str(size) + ' since no shapes are available')
+            continue # ignore request sizes that cannot be placed
+        for space in spaces:
+            for b in bins:
+                if (b.canFit(space)):
+                    b.fitBest(space)
+                    fittedBin = b
+                    isFit = True
+                    break
+            if (isFit):
+                break
+        if (not isFit):
+            newBin = Bin(dimensions.values())
+            # simply choose the best space in this case
+            newBin.fitBest(spaces[0])
+            fittedBin = newBin
+            bins.append(newBin)
+        if (not fittedBin.testPossible(True, False)):
+            print('Impossible configuration:\n' + str(fittedBin) + '\n')
+            break
+
+    return bins
 
 def randomRequestsFFD(boundaries):
     # get shape candidates
@@ -354,8 +445,11 @@ def randomRequestsFFD(boundaries):
     for i in range(random.randint(1, 1000)):
         requestSizes.append(random.randint(1, total/boundaries[0]))
 
-    # get bin packing
-    bins = firstFitDecreasing(boundaries, requestSizes, candidates, 1.0)
+    # get flat bin packing
+    # bins = ffdFlat(boundaries, requestSizes, candidates, 1.0)
+
+    # get best metric bin packing
+    bins = ffdBest(dimensions, requestSizes, candidates, 1.0)
 
     # print the configuration
     print('Packing as follows:')
@@ -376,8 +470,7 @@ def randomRequestsFFD(boundaries):
     # (need to match each allocated space with request size)
 
     # check if bin configuration is possible for all bins
-    if (not all(b.testPossible(boundaries, False) for b in bins)):
+    if (not all(b.testPossible(True, False) for b in bins)):
         print('Not all bin configurations are possible ! Test FAILED')
-
 
 randomRequestsFFD([24, 24, 24])
