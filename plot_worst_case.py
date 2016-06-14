d310f27f87109fa49ac129135ce7f7371dc0341c
Matthias Kohl <matthias.kohl@hotmail.de>
2016-06-14 13:26:39 +0200

    adapted plot for worst-case example, added worst-case for convex, added some additional possible paragraphs

--- plot_worst_case.py
+++ plot_worst_case.py
@@ -3,6 +3,8 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from matplotlib import gridspec
+import numpy as np
 
 eps_space = 0.03
 eps_time = 0.05
@@ -17,20 +19,25 @@ actualRectangles = [((0, 0), 1, 1), ((0, 1), 1, 1), ((0, 2), 1/2, 1),
 actualText = [(0, 4 + eps_time, 'j0'), (1/2, 1/2, 'j1'), (1/2, 3/2, 'j2'), (1/4, 5/2, 'j3'), (3/4, 5/2, 'j4')]
 
 patterns = ['/', '\\', '.', 'o', '*']
-fig, axarr = plt.subplots(2, sharex=True)
-axarr[0].set_ylim(0, 2)
-axarr[0].set_ylabel('Time', fontweight='bold')
-axarr[0].set_title('Optimal non-convex schedule', fontweight='bold')
+gs = gridspec.GridSpec(2, 1, height_ratios=[2, 5])
+ax1 = plt.subplot(gs[0])
+ax1.set_ylim(0, 2)
+ax1.set_ylabel('Time', fontweight='bold')
+ax1.set_title('Optimal non-convex schedule', fontweight='bold')
 for i, rect in enumerate(optRectangles):
-    axarr[0].add_patch(patches.Rectangle(rect[0], rect[1], rect[2], hatch=patterns[i], fill=False))
-    axarr[0].text(optText[i][0], optText[i][1], optText[i][2], bbox={'facecolor':'red'})
+    ax1.add_patch(patches.Rectangle(rect[0], rect[1], rect[2], fill=False))
+    ax1.text(optText[i][0], optText[i][1], optText[i][2], bbox={'facecolor':'red'})
+ax1.yaxis.set_ticks(np.arange(0, 3, 1))
 
-axarr[1].set_ylim(0, 5)
-axarr[1].set_ylabel('TIme', fontweight='bold')
-axarr[1].set_xlabel('Processors', fontweight='bold')
-axarr[1].set_title('Schedule produced by proposed algorithm', fontweight='bold')
+ax2 = plt.subplot(gs[1], sharex=ax1)
+ax2.set_ylim(0, 5)
+ax2.set_ylabel('TIme', fontweight='bold')
+ax2.set_xlabel('Processors', fontweight='bold')
+ax2.set_title('Schedule produced by proposed algorithm', fontweight='bold')
 for i, rect in enumerate(actualRectangles):
-    axarr[1].add_patch(patches.Rectangle(rect[0], rect[1], rect[2], hatch=patterns[i], fill=False))
-    axarr[1].text(actualText[i][0], actualText[i][1], actualText[i][2], bbox={'facecolor':'red'})
+    ax2.add_patch(patches.Rectangle(rect[0], rect[1], rect[2], fill=False))
+    ax2.text(actualText[i][0], actualText[i][1], actualText[i][2], bbox={'facecolor':'red'})
+ax2.yaxis.set_ticks(np.arange(0, 6, 1))
 
+plt.tight_layout()
 plt.show()
